c2af5e0ec9790b3a7915b3534aa2095fb3227f79
blitz-23 <94664667+blitz-23@users.noreply.github.com>
2022-10-24 06:09:46 +0530

    Delete rest directory
    Removing the directory to re upload the newer version

--- rest/src/main/java/com/example/crud/repo/AlienRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repo;
-
-import com.example.crud.model.Aien;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlienRepo extends CrudRepository<Aien, Integer> {
-}
